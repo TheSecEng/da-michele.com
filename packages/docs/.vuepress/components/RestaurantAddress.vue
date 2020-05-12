f2c8f95c425508529b2b71e73933dcf65bd3c27d
Johannes Rappen <jrappen@outlook.com>
2020-05-12 17:40:33 +0200

    fix: Fix RestaurantAddress component

--- packages/docs/.vuepress/components/RestaurantAddress.vue
+++ packages/docs/.vuepress/components/RestaurantAddress.vue
@@ -1,28 +1,31 @@
 <template>
-  <address class="address">
-    Ristorante Da Michele Baden-Baden<br>
-    Inh. Michele Romano<br>
-    Sinzheimer Str. 4<br>
-    76532 Baden-Baden<br>
+  <div class="address">
+    <address>
+      Ristorante Da Michele Baden-Baden<br>
+      Inh. Michele Romano<br>
+      Sinzheimer Str. 4<br>
+      76532 Baden-Baden<br>
+      <br>
+      Email: <a href="mailto:info@da-michele.com">info@da-michele.com</a><br>
+      Tel: <a href="tel:+49722161541">+49 - (0) 7221 - 61541</a>
+    </address>
     <br>
-    Email: <a href="mailto:info@da-michele.com">info@da-michele.com</a><br>
-    Tel: <a href="tel:+49722161541">+49 - (0) 7221 - 61541</a>
-  </address>
-  <br>
-  <p>
-    <a
-      href="https://www.google.com/maps/dir//48.786975,8.1936724/@48.786975,8.193672,19z?hl=de"
-    >Google Maps</a>
-    &nbsp;|&nbsp;
-    <a
-      href="https://share.here.com/l/48.786975,8.1936724"
-    >Here Maps</a>
-    &nbsp;|&nbsp;
-    <a
-      href="https://www.bing.com/maps?cp=48.786975~8.1936724&lvl=19"
-    >Bing Maps</a>
-  </p>
+    <p>
+      <a
+        href="https://www.google.com/maps/dir//48.786975,8.1936724/@48.786975,8.193672,19z?hl=de"
+      >Google Maps</a>
+      &nbsp;|&nbsp;
+      <a
+        href="https://share.here.com/l/48.786975,8.1936724"
+      >Here Maps</a>
+      &nbsp;|&nbsp;
+      <a
+        href="https://www.bing.com/maps?cp=48.786975~8.1936724&lvl=19"
+      >Bing Maps</a>
+    </p>
+  </div>
 </template>
+
 <style lang="stylus">
 .address
   padding 0.5em
