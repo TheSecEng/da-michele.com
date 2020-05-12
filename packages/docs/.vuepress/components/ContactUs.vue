acbb15951f9958982a57e37eb3b0e37c861a3cc5
Johannes Rappen <jrappen@outlook.com>
2020-05-12 18:13:09 +0200

    fix: Update ContactUs component

--- packages/docs/.vuepress/components/ContactUs.vue
+++ packages/docs/.vuepress/components/ContactUs.vue
@@ -1,7 +1,12 @@
 <template>
-  Contact us at:<br>
-  <RestaurantAddress/>
+  <div class="contact-us">
+    Contact us at:<br>
+    <RestaurantAddress/>
+  </div>
 </template>
 
 <style lang="stylus">
+.contact-us
+  padding 0.5em
+  margin 1em 0 !important
 </style>
