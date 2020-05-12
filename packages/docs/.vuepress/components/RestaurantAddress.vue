986a41b1abdfd7e2b8e6159ebdec2375f137b822
Johannes Rappen <jrappen@outlook.com>
2020-05-12 17:22:13 +0200

    fix: Fix RestaurantAddress component

--- packages/docs/.vuepress/components/RestaurantAddress.vue
+++ packages/docs/.vuepress/components/RestaurantAddress.vue
@@ -8,23 +8,17 @@
     Email: <a href="mailto:info@da-michele.com">info@da-michele.com</a><br>
     Tel: <a href="tel:+49722161541">+49 - (0) 7221 - 61541</a><br>
     <br>
-    <ul>
-      <li>
-        <a
-          href="https://www.google.com/maps/dir//48.786975,8.1936724/@48.786975,8.193672,19z?hl=de"
-        >Google Maps</a>
-      </li>
-      <li>
-        <a
-          href="https://wego.here.com/directions/mix//Ristorante-da-Michele,-Sinzheimer-Stra%C3%9Fe-4,-76532-Baden-Baden?map=48.78701,8.19363,19,normal"
-        >Here Maps</a>
-      </li>
-      <li>
-        <a
-          href="https://www.bing.com/maps?cp=48.787066~8.19306&lvl=19"
-        >Bing Maps</a>
-      </li>
-    </ul>
+    <a
+      href="https://www.google.com/maps/dir//48.786975,8.1936724/@48.786975,8.193672,19z?hl=de"
+    >Google Maps</a>
+    &nbsp;|&nbsp;
+    <a
+      href="https://share.here.com/l/48.78701,8.19363"
+    >Here Maps</a>
+    &nbsp;|&nbsp;
+    <a
+      href="https://www.bing.com/maps?cp=48.787066~8.19306&lvl=19"
+    >Bing Maps</a>
   </address>
 </template>
 <style lang="stylus">
