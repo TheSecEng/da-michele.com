a985b561c4f62545706a412dfb671f172f055f01
Johannes Rappen <jrappen@outlook.com>
2020-05-16 12:09:59 +0200

    fix: Fix size of GitHub social button

--- packages/docs/.vuepress/components/ContactButtons.vue
+++ packages/docs/.vuepress/components/ContactButtons.vue
@@ -43,6 +43,8 @@
         <svg
           role="img"
           viewBox="0 0 24 24"
+          width="16"
+          height="16"
           xmlns="http://www.w3.org/2000/svg"
         >
           <title>GitHub icon</title>
